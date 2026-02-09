f17944755619c7d0094e46ec13bb19c1ac884882
Santosh Singh <singh.santosh786@gmail.com>
2026-02-09 08:25:03 +0530

    formatted page as per shopify design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_04_072531) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_06_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -74,6 +74,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_04_072531) do
     t.datetime "completed_at"
     t.text "console_logs"
     t.datetime "created_at", null: false
+    t.text "dom_checks_data"
     t.text "error_message"
     t.text "html_snapshot"
     t.text "js_errors"
