1f415e87cf8551bed9dfea25a9a14a4ae4ac1aef
Santosh Singh <singh.santosh786@gmail.com>
2026-03-14 23:34:01 +0530

    reply_to email added as prowl@lucyapps.com

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "Prowl <alerts@prowl.lucyapps.com>"
+  default from: "Prowl <alerts@prowl.lucyapps.com>",
+          reply_to: "prowl@lucyapps.com"
   layout "mailer"
 end
